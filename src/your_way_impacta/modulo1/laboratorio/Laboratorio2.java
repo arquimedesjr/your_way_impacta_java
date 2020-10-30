67e42ba86c26f47492cbc9c67ee06ae9cbc384db
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-10-30 18:13:21 -0300

    Laborario

--- src/your_way_impacta/modulo1/laboratorio/Laboratorio2.java
+++ src/your_way_impacta/modulo1/laboratorio/Laboratorio2.java
@@ -4,5 +4,11 @@ public class Laboratorio2 {
 
     public static void main(String[] args) {
 
+        int valor = 100;
+
+        String par_impar = valor%2==0?"Par":"Impar";
+
+        System.out.println(par_impar);
+
     }
 }
