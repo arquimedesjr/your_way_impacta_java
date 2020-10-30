50717cf04a66454bd88f3308d9a325dfb4c515c8
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-10-30 17:42:37 -0300

    Laborario

---
+++ src/your_way_impacta/modulo1/laboratorio/Laboratorio1.java
@@ -0,0 +1,14 @@
+package your_way_impacta.modulo1.laboratorio;
+
+public class Laboratorio1 {
+    public static void main(String[] args) {
+        int valor1, valor2, resultado;
+
+        valor1 = 10;
+        valor2 = 20;
+
+        resultado = valor1 + valor2;
+
+        System.out.println(valor1+" + "+valor2 +" = "+resultado);
+    }
+}
---
+++ src/your_way_impacta/modulo1/laboratorio/Laboratorio2.java
@@ -0,0 +1,8 @@
+package your_way_impacta.modulo1.laboratorio;
+
+public class Laboratorio2 {
+
+    public static void main(String[] args) {
+
+    }
+}
