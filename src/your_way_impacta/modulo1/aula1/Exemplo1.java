fe29ca1844004c96c8a9f3f06f6422ac4c1ff94f
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-10-30 17:39:23 -0300

    Aula 1 - Introdução a linguagem Java

---
+++ src/your_way_impacta/modulo1/aula1/Exemplo1.java
@@ -0,0 +1,8 @@
+package your_way_impacta.modulo1.aula1;
+
+public class Exemplo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro programa em Java!");
+    }
+}
