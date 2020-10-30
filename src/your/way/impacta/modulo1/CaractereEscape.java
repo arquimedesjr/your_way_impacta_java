f4d7331a26aba3dac50858a546902c9047fac3aa
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-10-30 15:27:56 -0300

    init

---
+++ src/your/way/impacta/modulo1/CaractereEscape.java
@@ -0,0 +1,10 @@
+package your.way.impacta.modulo1;
+
+public class CaractereEscape {
+	public static void main(String args[]){
+		
+		System.out.println("Vai saltar uma linha \n saltou...");
+		System.out.println("Texto sem aspas, \"agora com aspas\"");
+		
+	}
+}
---
+++ src/your/way/impacta/modulo1/OlaMundo.java
@@ -0,0 +1,9 @@
+package your.way.impacta.modulo1;
+
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Ola, Mundo!!!");
+
+    }
+}
